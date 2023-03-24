7a76266572c03647e750941e7008616418b3733c
Blue Fire <contact@blue-fire.xyz>
2023-03-24 12:55:32 +0000

    Update & publish new doc versions

--- docs/main/_static/apps/examples/main.dart.js
+++ docs/main/_static/apps/examples/main.dart.js
@@ -83296,7 +83296,7 @@ a4(a){return A.dQ(null,null,1/a.aq(t.l).f.b)}}
 A.Yi.prototype={
 a4(a){var s,r=a.aq(t.l).f,q=r.f,p=q.b+8,o=this.c,n=o.b,m=n>=p+8+43,l=26+q.a,k=r.a.a-q.c-26,j=new A.k(A.W(o.a,l,k),n-8-p)
 n=this.d
-s=new A.k(A.W(n.a,l,k),n.b-8+p)
+s=new A.k(A.W(n.a,l,k),n.b+8-p)
 q=m?j:s
 return new A.ca(new A.aD(8,p,8,8),new A.mG(new A.a7H(j,s,m),new A.QB(q,A.bqp(this.e),m,A.bEn(),null),null),null)}}
 A.ab4.prototype={
