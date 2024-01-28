4e12c67a249f77e089cb36a33c9f6708730538a2
Blue Fire <contact@blue-fire.xyz>
2024-01-28 15:39:29 +0000

    Update & publish new doc versions

--- docs/1.11.0/_static/apps/tutorials-klondike-app/main.dart.js
+++ docs/1.11.0/_static/apps/tutorials-klondike-app/main.dart.js
@@ -12663,7 +12663,7 @@ $1(a){var s=$.aG
 s=(s==null?$.aG=A.cg(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/6e2ea58a5ccbefcfadc2d4b7b8045fc6c20ab812/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/f40e976bedff57e69e1b3d89a7c2a3c617a03dad/":s)+a},
 $S:46}
 A.Lj.prototype={
 $1(a){this.a.remove()
--- docs/1.8.2/_static/apps/flame-examples/main.dart.js
+++ docs/1.8.2/_static/apps/flame-examples/main.dart.js
@@ -19907,7 +19907,7 @@ $1(a){var s=$.bw
 s=(s==null?$.bw=A.e0(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/6e2ea58a5ccbefcfadc2d4b7b8045fc6c20ab812/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/f40e976bedff57e69e1b3d89a7c2a3c617a03dad/":s)+a},
 $S:80}
 A.YF.prototype={
 $1(a){this.a.remove()
